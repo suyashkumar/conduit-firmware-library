232f238919f03f2e545be9fde465ebfdd8f587bd
Suyash Kumar <suyashkumar2003@gmail.com>
2018-03-17 19:10:17 -0400

    Conduit V2 functionality

--- Conduit.cpp
+++ Conduit.cpp
@@ -12,166 +12,94 @@ device and decide what funciton to all is abstracted away entirely by this libra
 
 #include "Conduit.h"
 
-#define PREFIXED_NAME_CHARS   35
-#define STATIC_STRING_CHARS   64
-#define RECONNECT_DELAY_MS    5000
-#define CONNECT_DELAY_MS      500
-#define ROOT_NODE_NAME        "ROOT"
-
-// These values are required for interop with Conduit (conduit.suyash.io)!
-// DO NOT CHANGE THESE
-#define DEVICE_PATH_SUFFIX    "/device"
-#define STREAM_PATH_INFIX     "/stream/"
-#define DEFAULT_TOPIC_NAME    "outTopic"
-#define ON_CONNECT_MSG        "!connected"
-#define SERVER_DEFAULT_PORT   1883  
-
-#define STRCMP_SAME           0
-
-typedef struct node {
-  handler f;
-  const char* name;
-  node *next;
-} node_t;
-
-node_t* root;
-node_t* current;
-char prefixed_name[PREFIXED_NAME_CHARS];
+char prefixed_name[45];
+char api_key_topic[100];
+char sid[30];
 WiFiClient client;
-PubSubClient pClient(client);
+SocketIoClient webSocket;
+ESP8266WiFiMulti WiFiMulti;
 
-Conduit::Conduit(const char* name, const char* server, const char* prefix){
-  // Set name and server
-  this->_name = name;
-  this->_mqtt_server = server; 
-  this->_prefix = prefix;
+Conduit::Conduit(const char* name, const char* server, const char* firmware_key){
+    // Set name and server
+    this->_name = name;
+    this->_conduit_server = server;
+    this->_firmware_key = firmware_key;
 
-  // Compute _prefixed_name
-  strcpy(prefixed_name, prefix);
-  strcat(prefixed_name, name);
-  const char* full_prefixed_name = prefixed_name;
-  this->_prefixed_name = full_prefixed_name; 
+    // Compute _prefixed_name
+    strcpy(prefixed_name, "\"");
+    strcat(prefixed_name, firmware_key);
+    strcat(prefixed_name, "_");
+    strcat(prefixed_name, name);
+    strcat(prefixed_name, "\"");
+    const char* full_prefixed_name = prefixed_name;
+    this->_prefixed_name = full_prefixed_name;
+    Serial.print("Full prefixed name");
+    Serial.print(this->_prefixed_name);
 
-  // Init linked list
-  root = (node_t *)malloc(sizeof(node_t));
-  root->next = nullptr;
-  root->name = ROOT_NODE_NAME; // reserved name for root node (for now)
-  current = root; 
 }
 
 void Conduit::addHandler(const char* name, handler f){
-  node *newNode = (node_t*) malloc(sizeof(node_t));
-  newNode->f = f;
-  newNode->next = nullptr;
-  newNode->name = name;
-  current->next = newNode;
-  current = newNode;
+    this->_f_map[name] = f;
 }
 
 void Conduit::callHandler(const char* name){
-  node_t* currentInSearch = root;
-  while(true){
-    if (strcmp(name, currentInSearch->name) == STRCMP_SAME){
-      currentInSearch->f(); // Call function assoc with handler
-      break;
-    }
-    if(currentInSearch->next == nullptr){
-      break;
-    }
-    currentInSearch = currentInSearch->next;
-  }
+    this->_f_map[name]();
 }
 
 Conduit& Conduit::init(){
-  this->_client = &pClient;
-  pClient.setServer(this->_mqtt_server, SERVER_DEFAULT_PORT);
-  Serial.println("Happened");
-  pClient.setCallback([&](char* topic, byte* payload, unsigned int length){
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
-    char payloadStr[length];
-    for (int i = 0; i < length; i++) {
-      payloadStr[i] = (char)payload[i];
+    this->_client = &webSocket;
+    Serial.println("1");
+    std::function<void (const char*, size_t)> setSID = [this](const char * payload, size_t length) -> void {
+        this->_sid = sid;
+        strcpy(sid, payload);
+    };
+    std::function<void (const char*, size_t)> onCall = [this](const char * payload, size_t length) -> void {
+        this->callHandler(payload);
+    };
+    this->_client->on("id_message", setSID);
+    this->_client->on("server_directives", onCall);
+    this->_client->begin(this->_conduit_server, 8000, "/socket.io/?transport=websocket");
+
+    while (this->_sid == nullptr) {
+        webSocket.loop();
+        delay(200);
     }
-    payloadStr[length] = 0; // Null terminate payloadStr
-    removeSpace(payloadStr);
-    Serial.println(payloadStr);
-    this->callHandler(payloadStr); // Call function assoc with this handler
-  });
-  return *this;
+
+    strcpy(api_key_topic, this->_sid);
+    strcat(api_key_topic, "_api_key");
+
+    this->_client->emit(api_key_topic, this->_prefixed_name);
+    webSocket.loop();
+    return *this;
 }
 
-void Conduit::handle(){
-  if (!this->_client->connected()){
-    this->reconnect();
-  }
+void Conduit::handle() {
   this->_client->loop();
 }
 
-void Conduit::reconnect() {
-  // Loop until we're reconnected
-  while (!this->_client->connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-	Serial.println(this->_prefixed_name);
-    if (this->_client->connect(this->_prefixed_name)) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      this->_client->publish(DEFAULT_TOPIC_NAME, ON_CONNECT_MSG);
-      // Suscribe to topics:
-      this->_client->subscribe(this->_prefixed_name); // suscribe to events meant for this device
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(this->_client->state());
-      Serial.println(" try again in 5 seconds"); 
-      // Wait 5 seconds before retrying
-      delay(RECONNECT_DELAY_MS);
-    }
-  }
-}
-
-void Conduit::publishMessage(const char* message){
-  char str[STATIC_STRING_CHARS];
-  strcpy(str, this->_prefixed_name);
-  strcat(str, DEVICE_PATH_SUFFIX);
-  const char* topicName = str;
-  this->_client->publish(topicName, message);
-}
-
-void Conduit::publishData(const char* message, const char* dataStream) { 
-	char topicBuffer[STATIC_STRING_CHARS];
-	strcpy(topicBuffer, this->_prefixed_name);
-	strcat(topicBuffer, STREAM_PATH_INFIX);
-	strcat(topicBuffer, dataStream);
-	const char* topicName = topicBuffer;
-	this->_client->publish(topicName, message);
-}
-
 void Conduit::startWIFI(const char* ssid, const char* password){
-  WiFi.begin(ssid, password);
+    WiFiMulti.addAP(ssid, password);
   Serial.println("");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(CONNECT_DELAY_MS);
-    Serial.print(".");
+  while (WiFiMulti.run() != WL_CONNECTED) {
+	delay(500);
+	Serial.print(".");
   }
 
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 
 }
 
 
 void removeSpace(char* s) {
-    for (char* s2 = s; *s2; ++s2) {
-        if (*s2 != ' ')
-            *s++ = *s2;
-    }
-    *s = 0;
+	for (char* s2 = s; *s2; ++s2) {
+		if (*s2 != ' ')
+			*s++ = *s2;
+	}
+	*s = 0;
 }
 
